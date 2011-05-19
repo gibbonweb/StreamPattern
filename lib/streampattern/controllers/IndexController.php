465c3906db2518b35ab440d431e7b0d9d6d14326
Johannes Becker <jo.becker@gmail.com>
2011-05-19 12:53:12 +0200

    modified IndexController for ItemBean

--- lib/streampattern/controllers/IndexController.php
+++ lib/streampattern/controllers/IndexController.php
@@ -13,7 +13,7 @@ use hydrogen\view\View;
 
 class IndexController extends Controller {
 	
-	const ARTICLES_PER_PAGE = 10;
+	const ARTICLES_PER_PAGE = 16;
 	
 	public function index($page=1) {
 		// Calculate which blog post we should start with, based on the page.
@@ -21,12 +21,12 @@ class IndexController extends Controller {
 		
 		// Contact our ArticleModel and ask it for a summary of a set of posts.
 		$am = ArticleModel::getInstance();
-		$posts = $am->getSummaryCached($start, self::ARTICLES_PER_PAGE);
+		$items = $am->getSummary(0, self::ARTICLES_PER_PAGE);
 		
 		// Pass this data, as well as some other necessities, to the view.
 		View::load('index', array(
 			'title' => Config::getRequiredVal('general', 'site_title'),
-			'posts' => $posts
+			'items' => $items
 		));
 	}
 }
